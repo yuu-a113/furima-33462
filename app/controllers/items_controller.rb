4b2c41ed03061d0f24a0312a89d30f53fb338294
yuu-a113 <76138324+yuu-a113@users.noreply.github.com>
2021-02-08 12:43:24 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,7 +26,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if item.update(item_params)
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
